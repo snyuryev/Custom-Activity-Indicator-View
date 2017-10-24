ab329e6941b56db0aa14fca65d2e85ae06d8412a
Sasha Akhavan-Zanjani <mailsasha@me.com>
2017-10-24 22:02:34 +0100

    Updated to Swift 4.

--- CustomActivityIndicatorViewTest/CustomActivityIndicatorView.swift
+++ CustomActivityIndicatorViewTest/CustomActivityIndicatorView.swift
@@ -10,91 +10,92 @@ import UIKit
 import QuartzCore
 
 class CustomActivityIndicatorView: UIView {
+  
+  // MARK - Variables
+  
+  lazy private var animationLayer : CALayer = {
+    return CALayer()
+  }()
+  
+  var isAnimating : Bool = false
+  var hidesWhenStopped : Bool = true
+  
+  // MARK - Init
+  
+  init(image : UIImage) {
+    let frame : CGRect = CGRect(x: 0.0, y: 0.0, width: image.size.width, height: image.size.height)
     
-    // MARK - Variables
+    super.init(frame: frame)
     
-    lazy private var animationLayer : CALayer = {
-        return CALayer()
-    }()
+    animationLayer.frame = frame
+    animationLayer.contents = image.cgImage
+    animationLayer.masksToBounds = true
     
-    var isAnimating : Bool = false
-    var hidesWhenStopped : Bool = true
+    self.layer.addSublayer(animationLayer)
     
-    // MARK - Init
+    addRotation(forLayer: animationLayer)
+    pause(layer: animationLayer)
+    self.isHidden = true
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK - Func
+  
+  func addRotation(forLayer layer : CALayer) {
+    let rotation : CABasicAnimation = CABasicAnimation(keyPath:"transform.rotation.z")
     
-    init(image : UIImage) {
-        let frame : CGRect = CGRectMake(0.0, 0.0, image.size.width, image.size.height)
-
-        super.init(frame: frame)
-
-        animationLayer.frame = frame
-        animationLayer.contents = image.CGImage
-        animationLayer.masksToBounds = true
-
-        self.layer.addSublayer(animationLayer)
-        
-        addRotation(forLayer: animationLayer)
-        pause(layer: animationLayer)
-        self.hidden = true
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    rotation.duration = 1.0
+    rotation.isRemovedOnCompletion = false
+    rotation.repeatCount = HUGE
+    rotation.fillMode = kCAFillModeForwards
+    rotation.fromValue = NSNumber(value: 0.0)
+    rotation.toValue = NSNumber(value: 3.14 * 2.0)
+    
+    layer.add(rotation, forKey: "rotate")
+  }
+  
+  func pause(layer : CALayer) {
+    let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+    
+    layer.speed = 0.0
+    layer.timeOffset = pausedTime
+    
+    isAnimating = false
+  }
+  
+  func resume(layer : CALayer) {
+    let pausedTime : CFTimeInterval = layer.timeOffset
+    
+    layer.speed = 1.0
+    layer.timeOffset = 0.0
+    layer.beginTime = 0.0
+    
+    let timeSincePause = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+    layer.beginTime = timeSincePause
+    
+    isAnimating = true
+  }
+  
+  func startAnimating () {
+    
+    if isAnimating {
+      return
     }
     
-    // MARK - Func
-    
-    func addRotation(forLayer layer : CALayer) {
-        let rotation : CABasicAnimation = CABasicAnimation(keyPath:"transform.rotation.z")
-        
-        rotation.duration = 1.0
-        rotation.removedOnCompletion = false
-        rotation.repeatCount = HUGE
-        rotation.fillMode = kCAFillModeForwards
-        rotation.fromValue = NSNumber(float: 0.0)
-        rotation.toValue = NSNumber(float: 3.14 * 2.0)
-        
-        layer.addAnimation(rotation, forKey: "rotate")
+    if hidesWhenStopped {
+      self.isHidden = false
     }
-
-    func pause(#layer : CALayer) {
-        let pausedTime = layer.convertTime(CACurrentMediaTime(), fromLayer: nil)
-        
-        layer.speed = 0.0
-        layer.timeOffset = pausedTime
-        
-        isAnimating = false
+    resume(layer: animationLayer)
+  }
+  
+  func stopAnimating () {
+    if hidesWhenStopped {
+      self.isHidden = true
     }
-
-    func resume(#layer : CALayer) {
-        let pausedTime : CFTimeInterval = layer.timeOffset
-        
-        layer.speed = 1.0
-        layer.timeOffset = 0.0
-        layer.beginTime = 0.0
-        
-        let timeSincePause = layer.convertTime(CACurrentMediaTime(), fromLayer: nil) - pausedTime
-        layer.beginTime = timeSincePause
-        
-        isAnimating = true
-    }
-
-    func startAnimating () {
-        
-        if isAnimating {
-            return
-        }
-        
-        if hidesWhenStopped {
-            self.hidden = false
-        }
-        resume(layer: animationLayer)
-    }
-
-    func stopAnimating () {
-        if hidesWhenStopped {
-            self.hidden = true
-        }
-        pause(layer: animationLayer)
-    }
+    pause(layer: animationLayer)
+  }
 }
+
